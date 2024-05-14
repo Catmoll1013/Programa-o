1a92edf91017021767344eea2db6ba993dee2252
Catmoll1013 <168452129+Catmoll1013@users.noreply.github.com>
2024-05-14 12:03:50 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,22 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Teste cat')
+st.title('Teste cat - datas')
 
 st.write("Tabela")
 
-lista_variavel = [10, 20, 30, 40] 
-lista_nomes = ['cat', 'moll', '19', '13/10']
+lista_data = [1310, 0101, 3005, 2006] 
+lista_nomes = ['niver cat', 'ano novo', 'corpus cristi', 'dia da amizade']
 
 texto = st.text_input("Digite um nome")
-variavel = float(st.text_input("Digite a variavel", "0"))
+data = float(st.text_input("Digite a data", "0"))
 if texto:
     lista_nomes.append(texto)
-    lista_variavel.append(variavel)
+    lista_data.append(data)
 
     dataframe = pd.DataFrame({
         'Nome': lista_nomes,
-        'Salário': lista_variavel
+        'Salário': lista_data
     })
 
     dataframe.style.highlight_max(axis=0)
